e83dbb1eb3b357f1d2f58ec02be03cf6873bcbeb
echopsudo <radar010311@gmail.com>
2026-04-11 10:26:47 +0000

    Update enum.py

--- enum.py
+++ enum.py
@@ -1,16 +1,23 @@
 import sys
 import requests
+import re
 
 print("Custom Tooling: Python Directory \n")
 
 def help():
-    print("Help")
+    print("Help for this script")
+    print("Usage: python3 SCRIPTNAME.py 127.0.0.1 wordlist_file.txt")
 
 def dir():
     try:
         if len(sys.argv) == 3:
-            ip = sys.argv[1]
-            wordlist_file = open(f"{sys.argv[2]}", "r")
+            ip_regex = r"^([0-9]{1,3}\.){3}[0-9]{1,3}$"
+            if re.match(ip_regex, sys.argv[1]):
+                ip = sys.argv[1]
+                wordlist_file = open(f"{sys.argv[2]}", "r")
+            else:
+                ip = sys.argv[2]
+                wordlist_file = open(f"{sys.argv[1]}", "r")
             wordlist = wordlist_file.read().splitlines()
         else:
             print("It looks like you didn't provide any arguments")
@@ -37,18 +44,16 @@ def dir():
         print("IP or Port is not opened!")
         sys.exit()
 
-        
-#make a mode to enumerate subdomains later 
+
+#make a mode to enumerate subdomains later
 mode = input("do you want to enumerate dir/sub: ")
 
 while True:
     if mode == "dir":
         dir()
-    elif mode == "sub":
-        sub()
+    #elif mode == "sub":
+        #sub()
     elif mode == "--help":
         help()
     else:
         print("invalid choice, see --help")
-
-
