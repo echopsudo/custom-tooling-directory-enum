62fcfcfa991a1810042c03dc0dbea38fe5945fb1
echopsudo <radar010311@gmail.com>
2026-04-15 05:57:53 +0000

    rewrite caesar_cipher.py
    rewrite

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,41 +1,19 @@
-import re
+print("ceasar cipher shifter")
 
-word = "hello"
-number = 1
-if number > 25:
-    shift = number - 26
-else:
-    shift = number
+word = "helloworld!"
+shifted_word = ""
+shift = 27
+shift = shift % 26
 
+for letters in word:
+    shifted = ord(letters) + shift
+    if shifted > 122:
+        shifted -= 26
+        shifted_word += chr(shifted)
+    elif letters.isalpha():
+        shifted_word += chr(shifted)
+    else:
+        shifted -= shift
+        shifted_word += chr(shifted)
 
-def ceaser_shift(letter):
-    capital_letter = r"[A-Z]"
-    try:
-        if re.match(capital_letter, letter):
-            shifted = (ord(letter) + 32) + shift
-        else:
-            shifted = ord(letter) + shift
-
-        if shifted > 122:
-            corrected = shifted - shift - 25
-            return chr(corrected)
-        else:
-            return chr(shifted)
-
-    except TypeError:
-        print("invalid characters!, skipping")
-
-
-def cshift():
-    try:
-        result = ""
-        for letter in word:
-            result += ceaser_shift(letter)
-
-        print(word, "is shifted by", number, "becomes",result)
-
-    except TypeError:
-        print("invalid settings!")
-
-cshift()
-
+print(shifted_word)
